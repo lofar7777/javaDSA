a43520859690f5dcc7280f8c5cd297f519d1d464
lofar7777 <143798329+lofar7777@users.noreply.github.com>
2025-12-10 23:30:20 +0530

    Implement array to linked list conversion method
    Added a method to convert an array into a linked list.

--- LinkedList.java
+++ LinkedList.java
@@ -55,3 +55,36 @@ public class LinkedList {
 // Disadvantages
 // 1. No direct access to elements
 // 2. Extra memory for pointers
+
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1, Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+
+public class LinkedList{
+    // A function to convert an array into a Linked List
+    public static Node ConvertArr2LL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i = 1; i<arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+public static void main(String[] args){
+    int[] arr = {12, 3, 5, 2, 7};
+    Node head = ConvertArr2LL(arr);
+    System.out.print(head.data);
+}
+    
