dc552e1aedc09097ef1951513023ab179d69c853
lofar7777 <143798329+lofar7777@users.noreply.github.com>
2026-01-01 10:25:47 +0530

    Implement recursive and backtracking number printing
    Added functions to print numbers using recursion and backtracking.

--- Recursion4.java
+++ Recursion4.java
@@ -1,5 +1,5 @@
-// To print numbers form 1 to N using Recursion
 public class Recursion4{
+  // Function to print numbers from 1 to N usring Recursion
   static void printNums(int i, int n){
     // Base case
     if(i>n){
@@ -8,9 +8,42 @@ public class Recursion4{
     System.out.println(i);
     printNum(i+1, n);
   }
+
+  //Function to print numbers from N to 1 using Recursion
+  public static void printNums(int n){
+    if(n<1){
+      return;
+    }
+    System.out.println(n);
+    printNums(n-1);
+  }
+
+  //Function to pritn numbers 1 to N using backtracking
+  public static void backtrack(int i, int n){
+    if(i<1){
+      return;
+    }
+    backtrack(i-1, n);
+    System.out.println(i);
+  }
+
+  // Function to print numbers N to 1 using backtracking
+  public static void backtrack2(int i, int n){
+    if(i>n){
+      return;
+    }
+    backtrack(i+1, n);
+    System.out.println(i);
+  }
+
+
+    
   public static void main(String[] args){
-    int i = 1, n = 4;
-    printNum(i, n);
+    // int i = 1, n = 4;
+    printNums(1, 10);
+    printNums(10);
+    backtrack(1, 10);
+    backtrack2(1, 10);
   }
 }
 
@@ -18,4 +51,4 @@ public class Recursion4{
 // 2
 // 3
 // 4
-
+// 
