efd462dd399889335e54d448df7c0b6b2da9f8e5
lofar7777 <143798329+lofar7777@users.noreply.github.com>
2026-02-17 21:55:40 +0530

    Optimize bubble sort with swapped flag and range reduction
    Added optimization to bubble sort by using a swapped flag and reducing the inner loop's range.

--- Dsa with c/bubbleSort.c
+++ Dsa with c/bubbleSort.c
@@ -4,16 +4,24 @@ int main(){
   int arr[] = {3, 5, 2,4, 6, 20, 11};
   int n = sizeof(arr)/sizeof(arr[0]);
   for(int i = 0; i<n; i++){
-    for(int j = 0; j<n-1; j++){
+    int swapped = 0;                  // we can use this swapped flag for optimization 
+                                      // if the condition in the inner loop is not true for any iteration of the loop, it means that the array is already sorted, so we don't need to check again. 
+                                      // so this reduces the time complexity
+    for(int j = 0; j<n-1-i; j++){     // using n-1-i for optimization because once the inner for loop is executed,
+                                      // the largest element gets to the end of the array, so we don't need to traverse it again and again.
     if(arr[j] >arr[j+1]){
-      // swap both elements
+      // swap both elements  
+      swapped++;
       int temp = arr[j];
       arr[j] = arr[j+1];
       arr[j+1] = temp;
       }
     }
+    if(swapped==0){
+        break;
+    } 
 }
   for(int i = 0; i<n; i++){
-    printf("%d", arr[i]);
+    printf("%d\t", arr[i]);
   }
 }
