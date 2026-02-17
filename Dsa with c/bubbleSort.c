2bb2a2c2a48a51ada62c160b0db1210fcd23b3a2
lofar7777 <143798329+lofar7777@users.noreply.github.com>
2026-02-17 21:34:48 +0530

    Add bubble sort implementation in bubbleSort.c
    Implement bubble sort algorithm to sort an array.

---
+++ Dsa with c/bubbleSort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  // bubble sort 
+  int arr[] = {3, 5, 2,4, 6, 20, 11};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  for(int i = 0; i<n; i++){
+    for(int j = 0; j<n; j++){
+    if(arr[j] >arr[j+1]){
+      // swap both elements
+      int temp = arr[j];
+      arr[j] = arr[j+1];
+      arr[j+1] = temp;
+      }
+    }
+}
+  for(int i = 0; i<n; i++){
+    printf("&d", arr[i]);
+  }
+}
