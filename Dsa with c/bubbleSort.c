ba14a47444edcdaa96423b0b12943475c15c61dc
lofar7777 <143798329+lofar7777@users.noreply.github.com>
2026-02-17 21:41:15 +0530

    Fix bubble sort loop and printf format

--- Dsa with c/bubbleSort.c
+++ Dsa with c/bubbleSort.c
@@ -4,7 +4,7 @@ int main(){
   int arr[] = {3, 5, 2,4, 6, 20, 11};
   int n = sizeof(arr)/sizeof(arr[0]);
   for(int i = 0; i<n; i++){
-    for(int j = 0; j<n; j++){
+    for(int j = 0; j<n-1; j++){
     if(arr[j] >arr[j+1]){
       // swap both elements
       int temp = arr[j];
@@ -14,6 +14,6 @@ int main(){
     }
 }
   for(int i = 0; i<n; i++){
-    printf("&d", arr[i]);
+    printf("%d", arr[i]);
   }
 }
